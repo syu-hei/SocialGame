cb0daeb7c0067252711d4349d5d791cd15b60f09
syu-hei <53079859+syu-hei@users.noreply.github.com>
2020-10-10 00:29:21 +0900

    future/gacha/unity

---
+++ Assets/CharacterItem.cs
@@ -0,0 +1,64 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterItem : MonoBehaviour
+{
+
+    [SerializeField]
+	Image characterAsset;
+
+	[SerializeField]
+	Image typeAsset;
+
+    public UserCharacterModel userCharacterModel;
+	private MasterCharacterModel masterCharacterModel;
+
+    void Start()
+	{
+		masterCharacterModel = MasterCharacter.GetMasterCharacter(userCharacterModel.character_id);
+		if (masterCharacterModel.character_id == 0) {
+			UnityEngine.Debug.LogError("キャラクターのマスターデータを設定してください。");
+			return;
+		}
+
+		//Resourcesから画像を読み込む場合
+		Sprite characterSprite = Resources.Load<Sprite>(masterCharacterModel.asset_id);
+		if (characterSprite == null) {
+			UnityEngine.Debug.LogError("キャラクターの画像を設定してください。");
+			return;
+		}
+		characterAsset.sprite = characterSprite;
+		Sprite typeSprite = Resources.Load<Sprite>("CharacterTypeFrame_" + masterCharacterModel.type);
+		if (typeSprite == null) {
+			UnityEngine.Debug.LogError("キャラクターのタイプ画像を設定してください。");
+			return;
+		}
+		typeAsset.sprite = typeSprite;
+	}
+
+	public void SellButtonEvent()
+	{
+		Action action = () => {
+			GameObject characterManagerObject = GameObject.Find("CharacterManager");
+			if (characterManagerObject == null) {
+				UnityEngine.Debug.LogError("CharacterManagerが存在しません。");
+				return;
+			}
+			CharacterManager characterManager = characterManagerObject.GetComponent<CharacterManager>();
+			if (characterManager == null) {
+				UnityEngine.Debug.LogError("CharacterManagerアタッチされていません。");
+				return;
+			}
+			characterManager.CharacterList.SetActive(false);
+			characterManager.Dialog.SetActive(true);
+			characterManager.DialogSellPoint.text = "× " + masterCharacterModel.sell_point;
+		};
+		UserProfileModel userProfileModel = UserProfile.Get();
+		if (string.IsNullOrEmpty(userProfileModel.user_id)) {
+			UnityEngine.Debug.LogError("TitleSceneを起動してユーザー登録を行ってください。");
+			return;
+		}
+		StartCoroutine(CommunicationManager.ConnectServer("character_sell", "&user_id=" + userProfileModel.user_id + "&id=" + userCharacterModel.id, action));
+	}
+}
---
+++ Assets/TutorialManager.cs
@@ -0,0 +1,49 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialManager : MonoBehaviour
+{
+
+    [SerializeField]
+	Text tutorialText;
+
+	[SerializeField]
+	GameObject tutorialButton;
+
+	private UserProfileModel userProfileModel;
+
+	void Start()
+    {
+		tutorialButton.SetActive(false);
+
+		userProfileModel = UserProfile.Get();
+		if (string.IsNullOrEmpty(userProfileModel.user_id)) {
+			tutorialText.text = "TitleSceneを起動して\nユーザー登録を\n行ってください。";
+		} else {
+			if (UserProfile.TUTORIAL_QUEST <= userProfileModel.tutorial_progress) {
+				tutorialText.text = "チュートリアルは\n完了しています。";
+			} else {
+				tutorialText.text = "これはチュートリアルの\n説明文です。";
+				tutorialButton.SetActive(true);
+			}
+		}
+	}
+
+	public void TutorialButtonEvent()
+	{
+		if (string.IsNullOrEmpty(userProfileModel.user_id)) {
+			return;
+		}
+
+		if (UserProfile.TUTORIAL_QUEST <= userProfileModel.tutorial_progress) {
+			return;
+		}
+
+		Action action = () => {
+			tutorialButton.SetActive(false);
+			tutorialText.text = "チュートリアルが\n完了しました。";
+		};
+		StartCoroutine(CommunicationManager.ConnectServer("quest_tutorial", "&user_id=" + userProfileModel.user_id, action));
+	}
+}
